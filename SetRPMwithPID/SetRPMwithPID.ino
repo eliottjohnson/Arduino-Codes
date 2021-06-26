c2f1d983868249f8d9aa423f5042f99f6d4fe29c
Eliott <58270004+eliottjohnson@users.noreply.github.com>
2021-06-27 00:33:42 +0200

    Cleaned up the code and added some comments

--- SetRPMwithPID/SetRPMwithPID.ino
+++ SetRPMwithPID/SetRPMwithPID.ino
@@ -1,3 +1,10 @@
+// Eliott Johnson
+// Code to control a 4.4:1 Metal Gearmotor 25Dx63L mm LP 6V with 48 CPR Encoder
+// with a TB9051FTG Single Brushed DC Motor Driver Carrier from Polulu
+//
+// It allows to automatically control de power needed to send to the pwm and the direction to
+// follow a target RPM.
+
 #include <util/atomic.h> // For the ATOMIC_BLOCK macro
 
 //Encoder signals to determine position
@@ -12,15 +19,11 @@
 
 volatile int RisingAintt = 0;
 volatile int RisingBintt = 0;
-volatile int FallingAintt = 0;
-volatile int FallingBintt = 0;
-
 
 void setup() {
   Serial.begin(9600);
   pinMode(ENCA,INPUT);
   pinMode(ENCB,INPUT);
-
   pinMode(PWM1, OUTPUT);
   pinMode(PWM2, OUTPUT);
   pinMode(EN, OUTPUT);
@@ -28,99 +31,78 @@ void setup() {
 
   attachInterrupt(digitalPinToInterrupt(ENCA),readEncoderAR,RISING);
   attachInterrupt(digitalPinToInterrupt(ENCB),readEncoderBR,RISING);
-
 }
-////////
-//LOOP//
-///////
-  int RisingA = 0;
+
+/////////////////////////////////////////////////////////////////////////////////////
+//                                     LOOP                                        //
+/////////////////////////////////////////////////////////////////////////////////////
+  int RisingA = 0; //Count the number of time the signal from encoder A has rose
   int RisingB = 0; 
   int TotalRisingEdgesCurr = 0;
   int TotalRisingEdgesPrev = 0;
   int DiffTotalRisingEdges = 0;
-  int interval = 100;
+  int interval = 70; //The refresh rate. A good value is between 50 and 100.
   float RPM = 0;
   float deltaT = 0;
-  int pwr = 0;
+  int pwr = 0; //Power
   
   // PID constants
-  float kp = 0.2;
-  float kd = 0.01;
-  float ki = 0.0;
-  // motor direction
-  int dir = 1;
-  int e;
-  long prevT = 0;
-  float eprev = 0;
+  float kp = 0.2; //proportional constant
+  float kd = 0.01; //derivatice constant
+  float ki = 0.0; //integral constant
+  
+  int dir = 1; //motor direction
+  int e; //error between targetRPM and actual RPM
+  long prevT = 0; //previous time
+  float eprev = 0; //previous error
   float eintegral = 0;
   
 void loop() {
+  int targetRPM = 750*sin(prevT/1e3);   
+  dir = 1;
+    if (targetRPM<0){
+      dir = -1;
+  }
 
-   int targetRPM = 750*sin(prevT/1e3);
-
-     
-      dir = 1;
-      if (targetRPM<0){
-        dir = -1;
-      }
-   
-   ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
     RisingA = RisingAintt;
     RisingB = RisingBintt;
   }
-
-  //Number of rising encoder edges per revolution is 105.6
-
-    long currT = millis();
-    if (currT - prevT > interval) {
+  
+  long currT = millis();
+  if (currT - prevT > interval) {
       deltaT = ((float) (currT - prevT))/( 1.0e3 );
       prevT = currT;
-  
+    
       TotalRisingEdgesCurr = RisingA+RisingB;
       DiffTotalRisingEdges = TotalRisingEdgesCurr - TotalRisingEdgesPrev;
       TotalRisingEdgesPrev = TotalRisingEdgesCurr;
        
-      RPM = (float)((60*DiffTotalRisingEdges)/(deltaT))/(105.6); 
-
-      //error
-      e = abs(targetRPM) - RPM;
+      RPM = (float)((60*DiffTotalRisingEdges)/(deltaT))/(105.6); //Number of rising encoder edges per revolution is 105.6
+    
+      e = abs(targetRPM) - RPM; //error
       
-
-      // derivative
-      float dedt = (e-eprev)/(deltaT);
-
-      // integral
-      eintegral = eintegral + e*deltaT;
+      float dedt = (e-eprev)/(deltaT); // derivative
+    
+      eintegral = eintegral + e*deltaT; // integral
       
-      pwr = pwr + kp*e + kd*dedt + ki*eintegral;
-
+      pwr = pwr + kp*e + kd*dedt + ki*eintegral; //PID 
       
-      if (pwr>255){
+      if (pwr>255){ //Max PWM value is 255
         pwr=255;
       }
-
-      
       Serial.print(targetRPM);
       Serial.print(" ");
       Serial.print(dir*RPM);
       Serial.println();
-   }
-  
-  
-
-    
-   // signal the motor
-   setMotor(dir,pwr);
-
-
-  
-  
-  
-
+  }    
+  // signal the motor
+  setMotor(dir,pwr);
 }
 
-//Functions
-
+/////////////////////////////////////////////////////////////////////////////////////
+//                                    FUNCTIONS                                    //
+/////////////////////////////////////////////////////////////////////////////////////
 void setMotor(int dir, int pwmVal){ //pwmVal from 0 to 255
   digitalWrite(EN, HIGH);
   digitalWrite(ENB, LOW);
